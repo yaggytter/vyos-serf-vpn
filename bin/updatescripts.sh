a6f7e605f6d40337c8bbf5d42105a7c2828979f9
YAGASAKI Akihiro <yaggytter@momiage.com>
2014-08-05 17:50:21 +0900

    delete git package install commands

--- bin/updatescripts.sh
+++ bin/updatescripts.sh
@@ -2,7 +2,9 @@
 
 source /opt/vyos-serf-vpn/bin/commonvars.sh
 
-sudo yum install git
+# sudo yum install git
+# sudo apt-get install git
+# sudo aptitude install git
 
 cd /opt
 git clone https://github.com/yaggytter/vyos-serf-vpn.git
