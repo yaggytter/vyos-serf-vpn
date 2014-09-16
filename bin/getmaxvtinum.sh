a837da576edf02aaa201c67684dcfb8c7fc007d1
YAGASAKI Akihiro <yaggytter@momiage.com>
2014-09-16 17:22:01 +0900

    debug: vti num >= 10 probrem

--- bin/getmaxvtinum.sh
+++ bin/getmaxvtinum.sh
@@ -1,6 +1,6 @@
 #!/bin/vbash
 ## get max vti number from VyOS configuration
-export VTIRET=$(run show configuration commands | grep "vti bind" | awk '{print $9}' | sed -e 's/vti//g' -e "s/'//g" | sort | uniq | tail -1)
+export VTIRET=$(run show configuration commands | grep "vti bind" | awk '{print $9}' | sed -e 's/vti//g' -e "s/'//g" | sort | uniq -n | tail -1)
 # export VTIRET=$(cat ./ipsec* |  grep "vti bind" | awk '{print $9}' | sed -e 's/vti//g' -e "s/'//g" | sort | uniq | tail -1)
 
 if [ "" = "${VTIRET}" ]
